5ac2edef06d3a7ac5dee1fda9c4f5236342fe09f
nandini <100255342+nandinigorkal@users.noreply.github.com>
2022-03-11 14:02:41 +0530

    Add files via upload

---
+++ springbootapp/src/test/java/com/phase5/aws/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phase5.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
